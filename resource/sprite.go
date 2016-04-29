751e635ec7c07674a3880dbb152108f1154defcb
Andreas Jonsson <mail@andreasjonsson.se>
2016-04-29 13:32:01 +0200

    Removed sprite compression code.
    Don’t think it is used on PC???

--- resource/sprite.go
+++ resource/sprite.go
@@ -6,20 +6,6 @@ Sprite sheet files start with a 2 byte integer telling the number of frames insi
 followed by the sprite dimensions as 1 byte width and height. Next is a list of all frames,
 starting with their y and x offset, followed by width and height, each as 1 byte value.
 Last comes the offset of the frame inside the file, stored as 4 byte integer.
-If the width times height is greater than the difference between this and the next
-offset, then the frame is compressed as specified below. Else it is to be read as a usual
-indexed 256 color bitmap.
-
-Sprites of the Mac version are often compressed with an RLE method. Only transparency
-is compressed and the compression is linewise.
-Lines are assembled by reading them blockwise, where each block starts with a single
-byte Head, that gives further instructions:
-
-	0x00 → EndOfLine
-	0xFF → EndOfFrame
-	0x80 & Head = 0 → Head-many uncompressed pixels follow
-	0x80 & Head != 0 → ((0x7F & Head) + 1)-many transparent pixels
-
 */
 
 package resource
@@ -80,29 +66,31 @@ func LoadSprites(arch *Archive, images Images) (Sprites, error) {
 				}
 			}
 
+			var (
+				img        *image.Paletted
+				prevOffset uint32 = 0xFFFFFFFF
+			)
+
 			for i, frame := range sprite.Frames {
-				nextOffset := uint32(len(data))
-				if i < len(sprite.Frames)-1 {
-					nextOffset = sprite.Frames[i+1].Offset
-				}
+				if frame.Offset == prevOffset {
 
-				rect := image.Rect(0, 0, int(frame.Width), int(frame.Height))
+					// NOTE This is strange... could it be compressed?
+					fmt.Fprintln(Logger, "Frame repeat:", file, i)
 
-				// Check if the frame is compressed.
-				if uint32(frame.Width)*uint32(frame.Height) > nextOffset-frame.Offset {
-					fmt.Fprintln(Logger, "Compressed frame:", file, i)
-
-					// TODO Implement this.
-
+					img = sprite.Imgs[i-1]
 				} else {
-					img := image.NewPaletted(rect, palette.Plan9)
-					if err := binary.Read(bytes.NewReader(data[frame.Offset:]), binary.LittleEndian, &img.Pix); err != nil {
+					rect := image.Rect(0, 0, int(frame.Width), int(frame.Height))
+					img = image.NewPaletted(rect, palette.Plan9)
+					pixReader := bytes.NewReader(data[frame.Offset:])
+
+					if err := binary.Read(pixReader, binary.LittleEndian, &img.Pix); err != nil {
 						return sprites, err
 					}
-
-					sprite.Imgs[i] = img
-					images[fmt.Sprintf("%s,%d", file, i)] = Image{X: int(frame.X), Y: int(frame.Y), Data: img}
 				}
+
+				prevOffset = frame.Offset
+				sprite.Imgs[i] = img
+				images[fmt.Sprintf("%s,%d", file, i)] = Image{X: int(frame.X), Y: int(frame.Y), Data: img}
 			}
 
 			sprites[file] = sprite
