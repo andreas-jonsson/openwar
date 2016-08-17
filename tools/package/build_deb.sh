9bd51f5681f841203bb93ef89473a3a7f3680e44
Unknown <mail@andreasjonsson.se>
2016-08-17 14:54:18 +0200

    Added build_deb script.

---
+++ tools/package/build_deb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=openwar_0.0.1-$TRAVIS_BUILD_NUMBER
+
+if [ -d "$DIR" ]; then rm -rf "$DIR"; fi
+cp -r "tools/package/openwar_0.0.1-x" $DIR
+
+mkdir -p $DIR/opt/openwar
+mkdir -p $DIR/usr/local/bin
+
+cp -r data $DIR/opt/openwar
+cp openwar $DIR/opt/openwar
+ln -s -T /opt/openwar/openwar $DIR/usr/local/bin/openwar
+
+rpl e34f19fc-199d-4fb9-b334-aed07b29a173 $TRAVIS_BUILD_NUMBER $DIR/DEBIAN/control
+
+dpkg-deb --build $DIR
