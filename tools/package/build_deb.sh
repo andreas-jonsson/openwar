90427edaa2da44a10e652a9422ef69e9ecfa827f
Unknown <mail@andreasjonsson.se>
2016-08-17 21:40:37 +0200

    Updated .deb package scripts.

--- tools/package/build_deb.sh
+++ tools/package/build_deb.sh
@@ -6,11 +6,9 @@ if [ -d "$DIR" ]; then rm -rf "$DIR"; fi
 cp -r "tools/package/openwar_0.0.1-x" $DIR
 
 mkdir -p $DIR/opt/openwar
-mkdir -p $DIR/usr/local/bin
 
 cp -r data $DIR/opt/openwar
 cp openwar $DIR/opt/openwar
-ln -s -T /opt/openwar/openwar $DIR/usr/local/bin/openwar
 
 rpl e34f19fc-199d-4fb9-b334-aed07b29a173 $TRAVIS_BUILD_NUMBER $DIR/DEBIAN/control
 
--- tools/package/openwar.rb
+++ tools/package/openwar.rb
@@ -1,8 +1,18 @@
 class Openwar < Formula
-  homepage "http://www.openwar.io"
-  url ""
-  sha1 ""
+    homepage "http://www.openwar.io"
+    url "https://github.com/andreas-jonsson/openwar/archive/master.zip"
+    sha1 ""
 
-  def install
-  end
+    depends_on 'go' => '1.6'
+    depends_on 'gtk+'
+    depends_on 'sdl2'
+    depends_on 'sdl2_mixer'
+
+    def install
+        system "go get"
+        system "go build openwar.go"
+
+        bin.install "openwar"
+        share.install "data"
+    end
 end
