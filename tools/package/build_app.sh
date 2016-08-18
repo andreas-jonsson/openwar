e53aa240c72a5d082130333b975ec9c303b7d92e
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-18 14:34:03 +0200

    Update build_app.sh

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -5,16 +5,10 @@ DIR=OpenWar.app
 if [ -d "$DIR" ]; then rm -rf "$DIR"; fi
 cp -r "tools/package/OpenWar.app" $DIR
 
-wget -q https://www.libsdl.org/release/SDL2-2.0.4.dmg
-7z x SDL2-2.0.4.dmg
-cp -r SDL2/SDL2.framework $DIR/Contents/Resources
-
-wget -q https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.dmg
-7z x SDL2_mixer-2.0.1.dmg
-cp -r SDL2_mixer/SDL2_mixer.framework $DIR/Contents/Resources
-
 mkdir $DIR/Contents/MacOS
 cp openwar $DIR/Contents/MacOS
 cp -r data $DIR/Contents/MacOS
+
+dylibbundler -od -b -x $DIR/Contents/MacOS/openwar -d $DIR/Contents/libs
 
 7z a -tzip OpenWar.zip OpenWar.app
