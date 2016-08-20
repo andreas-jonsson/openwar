9f2e5ff13570c0fb7a5f36340d0c962f2ed719c1
Unknown <mail@andreasjonsson.se>
2016-08-20 09:12:37 +0200

    Just some cleanup.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-rm -rf $GOPATH/pkg $GOPATH/bin
-
-wget -q https://git.gnome.org/browse/gtk-osx/plain/gtk-osx-build-setup.sh
-chmod +x gtk-osx-build-setup.sh
-
-./gtk-osx-build-setup.sh
-export PATH=~/.local/bin:$PATH
-jhbuild bootstrap
-jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core
-
-wget -q http://ftp.gnome.org/pub/gnome/sources/gtk-mac-bundler/0.7/gtk-mac-bundler-0.7.3.tar.xz
-tar xf gtk-mac-bundler-0.7.3.tar.xz
-cd gtk-mac-bundler-0.7.3
-make install
-cd ..
-
 jhbuild shell
 export PATH=$PREFIX/bin:~/.local/bin:$PATH
 
