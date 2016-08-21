7da88736bd2efec11249a6d3d14216f47b0dbe24
Unknown <mail@andreasjonsson.se>
2016-08-21 15:47:52 +0200

    Updated build-scripts.

--- tools/package/setup_jhbuild.sh
+++ tools/package/setup_jhbuild.sh
@@ -5,10 +5,10 @@ rm -rf $GOPATH/pkg $GOPATH/bin
 wget -q https://git.gnome.org/browse/gtk-osx/plain/gtk-osx-build-setup.sh
 chmod +x gtk-osx-build-setup.sh
 
-./gtk-osx-build-setup.sh > /dev/null 2>&1
+./gtk-osx-build-setup.sh > /dev/null
 export PATH=~/.local/bin:$PATH
-jhbuild bootstrap > /dev/null 2>&1
-jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core > /dev/null 2>&1
+jhbuild bootstrap > /dev/null
+jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core > /dev/null
 
 wget -q http://ftp.gnome.org/pub/gnome/sources/gtk-mac-bundler/0.7/gtk-mac-bundler-0.7.3.tar.xz
 tar xf gtk-mac-bundler-0.7.3.tar.xz
