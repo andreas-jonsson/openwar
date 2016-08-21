a195e91a4cdf6790a68b0239932cb4e18f98e50e
Unknown <mail@andreasjonsson.se>
2016-08-21 11:55:16 +0200

    Cutting down on logging.

--- tools/package/setup_jhbuild.sh
+++ tools/package/setup_jhbuild.sh
@@ -7,8 +7,8 @@ chmod +x gtk-osx-build-setup.sh
 
 ./gtk-osx-build-setup.sh
 export PATH=~/.local/bin:$PATH
-jhbuild bootstrap
-jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core
+jhbuild bootstrap | /dev/null
+jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core | /dev/null
 
 wget -q http://ftp.gnome.org/pub/gnome/sources/gtk-mac-bundler/0.7/gtk-mac-bundler-0.7.3.tar.xz
 tar xf gtk-mac-bundler-0.7.3.tar.xz
