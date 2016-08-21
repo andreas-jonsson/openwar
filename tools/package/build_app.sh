87a905a1a88aac473f819898c0b1361c4f47bf3e
Unknown <mail@andreasjonsson.se>
2016-08-21 20:04:09 +0200

    Updated OSX build-script.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PATH=$PREFIX/bin:~/.local/bin:$PATH
+export PKG_CONFIG_PATH=~/.local/usr/share/pkgconfig:$PKG_CONFIG_PATH
 
 export SDL_PREFIX=~/.local/usr
 ./tools/sdl_from_source.sh
