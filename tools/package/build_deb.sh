0cf70f4bdbdf58515f6bb4a67bacdf9d5cc94705
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-22 13:36:53 +0200

    Update build_deb.sh

--- tools/package/build_deb.sh
+++ tools/package/build_deb.sh
@@ -8,7 +8,7 @@ cp -r "tools/package/openwar_0.0.1-x" $DIR
 mkdir -p $DIR/usr/local/bin
 mkdir -p $DIR/usr/local/share/openwar
 
-cp -r data/* $DIR/usr/local/openwar
+cp -r data/* $DIR/usr/local/share/openwar
 cp openwar $DIR/usr/local/bin
 
 rpl e34f19fc-199d-4fb9-b334-aed07b29a173 $TRAVIS_BUILD_NUMBER $DIR/DEBIAN/control
