75b69fd9dba7aab393052544430478df2e404a6c
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-22 15:27:29 +0200

    Update build_app.sh

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -4,6 +4,9 @@ export PATH=$PREFIX/bin:~/.local/bin:$PATH
 export PKG_CONFIG_PATH=~/.local/usr/lib/pkgconfig:$PKG_CONFIG_PATH
 export SDL_PREFIX=~/.local/usr
 
+# This step was moved to here because it takes to long to just do it all in the setup script.
+jhbuild build meta-gtk-osx-core > jhbuild_log.txt 2>&1
+
 ./tools/sdl_from_source.sh > /dev/null
 
 rm -rf $GOPATH/pkg $GOPATH/bin
