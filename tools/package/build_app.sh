f41904567024f0fddccacda0f612f37cb14d447b
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-22 12:22:17 +0200

    Update build_app.sh

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -4,7 +4,7 @@ export PATH=$PREFIX/bin:~/.local/bin:$PATH
 export PKG_CONFIG_PATH=~/.local/usr/lib/pkgconfig:$PKG_CONFIG_PATH
 export SDL_PREFIX=~/.local/usr
 
-./tools/sdl_from_source.sh
+./tools/sdl_from_source.sh > /dev/null
 
 rm -rf $GOPATH/pkg $GOPATH/bin
 
