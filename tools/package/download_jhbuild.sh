8db513ea215cf65e6507ebb29dd97fe473b5105b
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-22 12:25:21 +0200

    Update download_jhbuild.sh

--- tools/package/download_jhbuild.sh
+++ tools/package/download_jhbuild.sh
@@ -3,6 +3,4 @@
 ./tools/dropbox/dropbox_uploader.sh download Public/jhbuild.zip
 export PATH=~/.local/bin:$PATH
 
-7z x jhbuild.zip
-mv -f jhbuild/* $HOME
-rm -rf jhbuild jhbuild.zip
+7z x jhbuild.zip -o~/ -aoa
