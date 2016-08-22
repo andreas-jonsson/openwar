0616c49ab0de0bd0f7af652a277cf13a5b578361
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-22 15:47:30 +0200

    Moved gtk-core build step.

---
+++ tools/package/revive_jhbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=~/.local/bin:$PATH
+
+# Ensure that we get some output at least every 5min.
+while :; do echo "ping!"; sleep 300; done &
+
+# This step was moved to here because it takes to long to just do it all in the setup script.
+jhbuild build meta-gtk-osx-core > jhbuild_log.txt 2>&1
