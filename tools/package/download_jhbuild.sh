57154fac4e155b445cb366ed84db25ea2a766b00
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-23 09:17:39 +0200

    Buildscript update.

--- tools/package/download_jhbuild.sh
+++ tools/package/download_jhbuild.sh
@@ -3,10 +3,10 @@
 ./tools/dropbox/dropbox_uploader.sh download Public/jhbuild.zip
 7z x jhbuild.zip -o~/ -aoa > /dev/null
 
-mv -f jhbuild/.cache ~/
-mv -f jhbuild/.jhbuild* ~/
-mv -f jhbuild/gtk ~/
-mv -f jhbuild/tmp-jhbuild-revision ~/
+mv -f jhbuild/.cache $HOME
+mv -f jhbuild/.jhbuild* $HOME
+mv -f jhbuild/gtk $HOME
+mv -f jhbuild/tmp-jhbuild-revision $HOME
 
 ls -al
 
