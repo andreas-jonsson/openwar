04e9efefef3752f5e57585cfb438b60c0ddffb83
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-23 08:39:41 +0200

    Updated extraction code.

--- tools/package/download_jhbuild.sh
+++ tools/package/download_jhbuild.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+export PATH=$PREFIX/bin:~/.local/bin:$PATH
+
 ./tools/dropbox/dropbox_uploader.sh download Public/jhbuild.zip
 7z x jhbuild.zip -o~/ -aoa > /dev/null
-mv -f jhbuild/* ~/
+
+mv -f jhbuild/.cache ~/
+mv -f jhbuild/.jhbuild* ~/
+mv -f jhbuild/gtk ~/
+mv -f jhbuild/tmp-jhbuild-revision ~/
+
+ls -al
 
 wget -q http://ftp.gnome.org/pub/gnome/sources/gtk-mac-bundler/0.7/gtk-mac-bundler-0.7.3.tar.xz
 tar xf gtk-mac-bundler-0.7.3.tar.xz
