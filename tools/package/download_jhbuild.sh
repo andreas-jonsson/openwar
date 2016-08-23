a98ca357ed156935a2a194e0bcf0dd49b80d9e3c
Andreas T Jonsson <mail@andreasjonsson.se>
2016-08-23 08:12:31 +0200

    Downloading gtk prebuilt.

--- tools/package/download_jhbuild.sh
+++ tools/package/download_jhbuild.sh
@@ -4,3 +4,10 @@
 export PATH=~/.local/bin:$PATH
 
 7z x jhbuild.zip -o~/ -aoa
+mv -f jhbuild/* ~/
+
+wget -q http://ftp.gnome.org/pub/gnome/sources/gtk-mac-bundler/0.7/gtk-mac-bundler-0.7.3.tar.xz
+tar xf gtk-mac-bundler-0.7.3.tar.xz
+cd gtk-mac-bundler-0.7.3
+make install
+cd ..
