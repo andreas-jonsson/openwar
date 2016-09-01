5f461e98d0bf8c668ab0d2fde91bf6bca181ca17
Andreas T Jonsson <mail@andreasjonsson.se>
2016-09-01 08:07:52 +0200

    Update build_app.sh

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$PREFIX/bin:~/.local/bin:/usr/local/bin:$PATH
-export PKG_CONFIG_PATH=~/lib/pkgconfig:$PKG_CONFIG_PATH
+export PATH=$PREFIX/bin:~/.local/bin:~/gtk/inst/bin:$PATH
+export PKG_CONFIG_PATH=~/lib/pkgconfig:~/gtk/inst/lib/pkgconfig:$PKG_CONFIG_PATH
 # export SDL_PREFIX=~/.local/usr
 
 # ./tools/sdl_from_source.sh > /dev/null
