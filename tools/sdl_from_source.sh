8e7411d7c4e2e99e554e0870aa7afb8260595f22
Andreas T Jonsson <mail@andreasjonsson.se>
2016-10-05 21:04:57 +0200

    Updated SDL

--- tools/sdl_from_source.sh
+++ tools/sdl_from_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q http://www.libsdl.org/release/SDL2-2.0.3.tar.gz
+wget -q http://www.libsdl.org/release/SDL2-2.0.4.tar.gz
 tar xf SDL2-*.tar.gz
 cd SDL2-* && ./configure --prefix=$SDL_PREFIX && make && sudo make install
 
