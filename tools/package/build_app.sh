42a92142059f0f6759a00718414967b7261ecfb1
Andreas T Jonsson <mail@andreasjonsson.se>
2016-10-11 13:56:37 +0200

    Updated dylib path.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -4,7 +4,7 @@ if [ -d OpenWar.app ]; then rm -rf OpenWar.app; fi
 cp -r "tools/package/OpenWar.app" OpenWar.app
 
 cp openwar OpenWar.app/Contents/MacOS
-dylibbundler -od -b -x OpenWar.app/Contents/MacOS/openwar -d OpenWar.app/Contents/lib
+dylibbundler -od -b -x OpenWar.app/Contents/MacOS/openwar -d OpenWar.app/Contents/libs
 
 zip -rq OpenWar.zip OpenWar.app
 rm -rf OpenWar.app
