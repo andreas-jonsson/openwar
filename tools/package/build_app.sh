d64f100a13ba46286d0349b8ab46e4f4b59bb9f9
Andreas T Jonsson <mail@andreasjonsson.se>
2016-10-11 13:15:11 +0200

    Fixed broken flags.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -7,10 +7,10 @@ mkdir OpenWar.app/Contents/Resources
 mkdir OpenWar.app/Contents/Frameworks
 
 wget -q https://dl.dropboxusercontent.com/u/1955192/SDL2.framework.zip
-unzip -dq OpenWar.app/Contents/Frameworks SDL2*.zip
+unzip -q -d OpenWar.app/Contents/Frameworks SDL2*.zip
 
 wget -q https://dl.dropboxusercontent.com/u/1955192/SDL2_mixer.framework.zip
-unzip -dq OpenWar.app/Contents/Frameworks SDL2_mixer*.zip
+unzip -q -d OpenWar.app/Contents/Frameworks SDL2_mixer*.zip
 
 cp openwar OpenWar.app/Contents/MacOS
 
