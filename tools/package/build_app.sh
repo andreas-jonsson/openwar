7c5b36c1b5100c937dd3a6d0d7b6ae764abc2f15
Andreas T Jonsson <mail@andreasjonsson.se>
2016-10-11 14:08:22 +0200

    Fixed deep dylib copy.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -5,6 +5,8 @@ cp -r "tools/package/OpenWar.app" OpenWar.app
 
 cp openwar OpenWar.app/Contents/MacOS
 dylibbundler -od -b -x OpenWar.app/Contents/MacOS/openwar -d OpenWar.app/Contents/libs
+dylibbundler -b -x OpenWar.app/Contents/libs/libSDL2-2.0.0.dylib -d OpenWar.app/Contents/libs
+dylibbundler -b -x OpenWar.app/Contents/libs/libSDL2_mixer-2.0.0.dylib -d OpenWar.app/Contents/libs
 
 zip -rq OpenWar.zip OpenWar.app
 rm -rf OpenWar.app
