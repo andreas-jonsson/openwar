da6aee565e4f3a0afd72697b6798f9e422f8f1ba
Andreas T Jonsson <mail@andreasjonsson.se>
2016-10-11 13:44:19 +0200

    Updated OSX bundlescript.

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -3,15 +3,8 @@
 if [ -d OpenWar.app ]; then rm -rf OpenWar.app; fi
 cp -r "tools/package/OpenWar.app" OpenWar.app
 
-mkdir OpenWar.app/Contents/Frameworks
-
-wget -q https://dl.dropboxusercontent.com/u/1955192/SDL2.framework.zip
-unzip -q -d OpenWar.app/Contents/Frameworks SDL2*.zip
-
-wget -q https://dl.dropboxusercontent.com/u/1955192/SDL2_mixer.framework.zip
-unzip -q -d OpenWar.app/Contents/Frameworks SDL2_mixer*.zip
-
 cp openwar OpenWar.app/Contents/MacOS
+dylibbundler -od -b -x OpenWar.app/Contents/MacOS/openwar -d OpenWar.app/Contents/lib
 
-zip -r OpenWar.zip OpenWar.app
+zip -rq OpenWar.zip OpenWar.app
 rm -rf OpenWar.app
