f92a8b36921b67a570a96cd51a1b15c6a7a12d79
Andreas T Jonsson <mail@andreasjonsson.se>
2017-01-17 11:01:52 +0100

    Updated headers.

--- platform/platform_mobile.go
+++ platform/platform_mobile.go
@@ -1,7 +1,7 @@
 // +build mobile
 
 /*
-Copyright (C) 2016 Andreas T Jonsson
+Copyright (C) 2016-2017 Andreas T Jonsson
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
