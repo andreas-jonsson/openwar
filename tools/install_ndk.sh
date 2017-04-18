46e565ddb72e97217e0f7f6a1975f0eb745a65de
Andreas T Jonsson <mail@andreasjonsson.se>
2017-04-18 11:36:33 +0200

    Update install_ndk.sh

--- tools/install_ndk.sh
+++ tools/install_ndk.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# curl -L http://dl.google.com/android/ndk/android-ndk-${NDK_VERSION}-linux-x86_64.bin -O
-# chmod u+x android-ndk-${NDK_VERSION}-linux-x86_64.bin
-# ./android-ndk-${NDK_VERSION}-linux-x86_64.bin > /dev/null
-
 curl -L http://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip -O
-unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip
-
-# rm android-ndk-${NDK_VERSION}-linux-x86_64.bin
+unzip android-ndk-${NDK_VERSION}-linux-x86_64.zip > /dev/null
 rm android-ndk-${NDK_VERSION}-linux-x86_64.zip
+
 export ANDROID_NDK_HOME=`pwd`/android-ndk-${NDK_VERSION}
 export PATH=${ANDROID_NDK_HOME}:${PATH}
