3e027cabbe7b459fc513baaac34ab02b24ec0143
Andreas T Jonsson <mail@andreasjonsson.se>
2017-04-19 08:45:01 +0200

    Update build_app.sh

--- tools/package/build_app.sh
+++ tools/package/build_app.sh
@@ -6,5 +6,5 @@ cp -r "tools/package/OpenWar.app" OpenWar.app
 cp openwar OpenWar.app/Contents/MacOS
 dylibbundler -od -b -x OpenWar.app/Contents/MacOS/openwar -d OpenWar.app/Contents/libs
 
-zip -rq OpenWar.zip OpenWar.app
+zip -rq openwar_$OPENWAR_VERSION_osx.zip OpenWar.app
 rm -rf OpenWar.app
